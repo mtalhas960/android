6b6c5072e9a3caeb6676db976d4929d5c907293a
Muhammad Talha <mtalhas960@gmail.com>
2024-10-22 13:03:36 +0500

    .

---
+++ src/data/experienceData.jsx
@@ -0,0 +1,20 @@
+export const experienceData = [
+    {
+      id: 1,
+      duration: "April 2024 - Present",
+      title: "FRONT-END DEVELOPER",
+      company: "Evonicsoft Private Ltd.",
+    },
+    {
+      id: 2,
+      duration: "July 2023 - December 2023",
+      title: "FRONT-END DEVELOPER",
+      company: "Corvit System Bahawalpur",
+    },
+    {
+      id: 3,
+      duration: "Jan 2023 - Present",
+      title: "SELF EMPLOYED",
+      company: "Code And Build Something Everyday",
+    },
+  ];
